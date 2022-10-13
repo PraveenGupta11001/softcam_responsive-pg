a10d9950b67921935ae9bff568e4c6ed81910645
PraveenGupta11001 <105053871+PraveenGupta11001@users.noreply.github.com>
2022-10-13 18:04:28 +0530

    softcam

---
+++ script.js
@@ -0,0 +1,11 @@
+let item1 = document.querySelector('.links__ph');
+let item2 = document.querySelector('.fa-bars');
+function showNav(){
+    item1.classList.toggle("hide");
+    item2.classList.toggle("addBorder");
+
+}
+
+// function refresh() {
+//     location.reload();
+// }
